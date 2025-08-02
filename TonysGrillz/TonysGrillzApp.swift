4ec5dbe052db1544e8fd7eed677d805b4f5cee79
FahimU313 <115724162+FahimU313@users.noreply.github.com>
2025-08-02 16:03:34 -0400

    Initial Commit

---
+++ TonysGrillz/TonysGrillzApp.swift
@@ -0,0 +1,17 @@
+//
+//  TonysGrillzApp.swift
+//  TonysGrillz
+//
+//  Created by Fahim Uddin on 8/2/25.
+//
+
+import SwiftUI
+
+@main
+struct TonysGrillzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
