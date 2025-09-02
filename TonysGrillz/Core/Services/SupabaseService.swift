6e90401474166301fa8ddcf6ed422e6484740dff
FahimU313 <115724162+FahimU313@users.noreply.github.com>
2025-09-02 08:53:11 -0400

    SUPABASE_KEY is now in a config fiel and works!

--- TonysGrillz/Core/Services/SupabaseService.swift
+++ TonysGrillz/Core/Services/SupabaseService.swift
@@ -12,6 +12,14 @@ import Supabase
 struct SupabaseService: BackendService {
     static let supabase = SupabaseClient(
         supabaseURL: URL(string: "https://eckedzdtzhfwtessshjm.supabase.co")!,
-        supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVja2VkemR0emhmd3Rlc3NzaGptIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTYyMTYzMzUsImV4cCI6MjA3MTc5MjMzNX0.YY8EFSs6pUFBdx66fd5nlEjuQGY3QBs_piqMED5qNYE"
+        supabaseKey: AppEnviroment.supabseKey
     )
 }
+
+
+//struct SupabaseService: BackendService {
+//    static let supabase = SupabaseClient(
+//        supabaseURL: URL(string: "https://eckedzdtzhfwtessshjm.supabase.co")!,
+//        supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVja2VkemR0emhmd3Rlc3NzaGptIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTYyMTYzMzUsImV4cCI6MjA3MTc5MjMzNX0.YY8EFSs6pUFBdx66fd5nlEjuQGY3QBs_piqMED5qNYE"
+//    )
+//}
